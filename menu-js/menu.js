85c53c13a9a87a79827a6eb78314188751d04027
Ester Suchanová <p.res@centrum.cz>
2021-05-28 17:38:47 +0200

    změna menu

--- menu-js/menu.js
+++ menu-js/menu.js
@@ -1,7 +1,17 @@
-function openNav() {
-  document.getElementsByClassName("menu__mobil--hidden").style.width = "250px";
+function myFunction() {
+  document.getElementById("menu__mobil--hidden").classList.toggle("show");
 }
 
-function closeNav() {
-  document.getElementsByClassName("menu__mobil--hidden").style.width = "0";
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menu__link')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
